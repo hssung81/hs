08f956e12093aed94cf22f04c38857bd09983ce6
hssung81 <hssung@kinx.net>
2020-04-01 11:54:19 +0900

    fix

--- dev/linux/go_env.sh
+++ dev/linux/go_env.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:jonathonf/vim
+sudo add-apt-repository ppa:jonathonf/vim -y
 sudo apt update
 sudo apt install vim -y
 
 wget https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz
 
+mkdir ~/golang
 tar -C ~/golang -xzf go1.14.1.linux-amd64.tar.gz
 
-echo "export PATH=$PATH:~/golang/bin
-export GOPATH=~/golang" >> ~/.profile
+echo "export PATH=$PATH:~/golang/go/bin
+export GOPATH=~/golang/go" >> ~/.profile
+source ~/.profile
 
 
 mkdir -p ~/.vim/autoload ~
@@ -39,7 +41,7 @@ apt-get install mercurial -y
 
 vim -c 'GoInstallBinaries' -c 'qa!'
 
-apt-get install python3-dev cmake
+apt-get install python3-dev cmake -y
 
 cd ~/.vim/bundle
 git clone https://github.com/Valloric/YouCompleteMe.git
